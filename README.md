c698e69eedbe9766acf23a980b0a3bad28e2f78f
SidneyXu <siriuseddy@gmail.com>
2016-01-10 21:38:30 +0800

    Update.

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # Advent-of-Code
 
-Solutions for the [Advent of Code](http://adventofcode.com/).
+My solutions for the[Advent of Code](http://adventofcode.com/) in multiple languages(Java, Groovy, Scala, Kotlin, Go and Ruby).
+
+## How to run
+
+Java
+
+```bash
+cd Java/day01
+javac Main.java 
+java Main
+```
+
+Groovy
+
+```bash
+cd Groovy/day01
+groovy main.groovy
+```
+
+Scala
+
+```bash
+cd Scala/day01
+scala main.scala
+```
+
+Kotlin
+
+```bash
+cd Kotlin/day01
+kotlinc -script main.kts
+```
+
+Go
+
+```bash
+cd Go/day01
+go run main.go
+```
+
+Ruby
+
+```bash
+cd Ruby/day01
+ruby main.rb 
+```
--- Scala/day03/main.scala
+++ Scala/day03/main.scala
@@ -1,11 +1,26 @@
 import scala.io.Source
 
-val santaHouses = Source.fromFile("input.txt").mkString
-  .split("")
-  .scanLeft((0, 0)) {
-  case (t, "^") => (t._1, t._2 + 1)
-  case (t, "v") => (t._1, t._2 - 1)
-  case (t, ">") => (t._1 + 1, t._2)
-  case (t, "<") => (t._1 - 1, t._2)
-}.distinct.length
-println(santaHouses)
+val houses = Source.fromFile("input.txt").mkString
+        .scanLeft((0, 0)) {
+            case (t, '^') => (t._1, t._2 + 1)
+            case (t, 'v') => (t._1, t._2 - 1)
+            case (t, '>') => (t._1 + 1, t._2)
+            case (t, '<') => (t._1 - 1, t._2)
+        }.distinct
+println(houses.size)
+
+var houses2 = Source.fromFile("input.txt").mkString
+        .zipWithIndex
+        .scanLeft(((0, 0), (0, 0))) {
+            case ((t1, t2), ('^', i)) if i % 2 == 0 => ((t1._1, t1._2 + 1), t2)
+            case ((t1, t2), ('^', i)) => (t1, (t2._1, t2._2 + 1))
+            case ((t1, t2), ('v', i)) if i % 2 == 0 => ((t1._1, t1._2 - 1), t2)
+            case ((t1, t2), ('v', i)) => (t1, (t2._1, t2._2 - 1))
+            case ((t1, t2), ('>', i)) if i % 2 == 0 => ((t1._1 + 1, t1._2), t2)
+            case ((t1, t2), ('>', i)) => (t1, (t2._1 + 1, t2._2))
+            case ((t1, t2), ('<', i)) if i % 2 == 0 => ((t1._1 - 1, t1._2), t2)
+            case ((t1, t2), ('<', i)) => (t1, (t2._1 - 1, t2._2))
+        }
+        .flatMap(it => List(it._1, it._2))
+        .distinct
+println(houses2.size)
